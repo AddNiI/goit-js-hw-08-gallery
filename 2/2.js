0771f4eb495f484ad4e3e2e7498bc22773a423b6
AddNiI <100078200+AddNiI@users.noreply.github.com>
2024-11-01 19:48:15 +0200

    отебитесь, заебали, дайте поспать, уроды!

--- 2/2.js
+++ 2/2.js
@@ -7,3 +7,17 @@ document.addEventListener('DOMContentLoaded', function() {
         image.style.transform = `scale(${scale})`;
     });
 });
+
+// Підключіть lodash
+// <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"></script>
+
+document.addEventListener('DOMContentLoaded', function() {
+    const box = document.getElementById('box');
+
+    const moveBox = _.debounce(function(event) {
+        box.style.left = event.clientX + 'px';
+        box.style.top = event.clientY + 'px';
+    }, 100);
+
+    document.addEventListener('mousemove', moveBox);
+});
